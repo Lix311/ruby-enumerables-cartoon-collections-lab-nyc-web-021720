1f269b689c7fcc20d62b5696543c2e3f5d1e317e
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-11 03:38:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,13 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.reduce{|x| }
+  array.reduce{|x| 
+    if x.length > 4
+      return true 
+    else 
+      return false
+    end 
+  }
 end
 
 def find_the_cheese# code an argument here
